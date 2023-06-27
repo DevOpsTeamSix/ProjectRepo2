b71f5acc7468f13bee56cbb21229e002510e71a1
Valentechno <valentino.agbetoglo.study@gmail.com>
2023-06-27 19:57:49 -0400

    SSH key resrouce added

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "jump_server" {
   ami             = "ami-024e6efaf93d85776"
   instance_type   = "t2.medium"
   security_groups = [module.security_group.sg_id]
+  key_name        = aws_key_pair.keypair.key_name
 
   tags = {
     Name = "JumpServer"
@@ -16,6 +17,7 @@ resource "aws_instance" "web_server2" {
   ami             = "ami-024e6efaf93d85776"
   instance_type   = "t2.micro"
   security_groups = [module.security_group.sg_id]
+  key_name        = aws_key_pair.keypair.key_name
 
   tags = {
     Name = "WebServer1"
@@ -26,6 +28,7 @@ resource "aws_instance" "web_server1" {
   ami             = "ami-024e6efaf93d85776"
   instance_type   = "t2.micro"
   security_groups = [module.security_group.sg_id]
+  key_name        = aws_key_pair.keypair.key_name
 
   tags = {
     Name = "WebServer2"
@@ -33,4 +36,19 @@ resource "aws_instance" "web_server1" {
 }
 module "security_group" {
   source = "./modules/security-groups"
+}
+
+resource "tls_private_key" "pemkey" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "keypair" {
+  key_name   = "practicekey"
+  public_key = tls_private_key.pemkey.public_key_openssh
+}
+
+resource "local_file" "key_local_file" {
+  content  = tls_private_key.pemkey.private_key_pem
+  filename = "practicekey.pem"
 }
